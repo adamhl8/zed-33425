ba51313b2d79841c00c415bc12fb04b9c0ca14db
Adam Langbert <adamhl@pm.me>
2025-06-26 09:15:21 -0500

    add comment

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+// The following ESLint errors don't show up in Zed >v0.192.5
+
 // ESLint Errors: @typescript-eslint/no-unused-vars
 for (const key of Object.keys(process.env)) {
   // ESLint Errors: no-await-in-loop, promise/avoid-new, no-promise-executor-return
